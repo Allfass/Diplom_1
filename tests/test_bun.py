1c41cf846982f3addb4fe8ee86620c3a0f6d59a9
Shamil <sh98allfass@gmail.com>
2024-07-06 19:23:42 +0500

    covered bun

---
+++ tests/test_bun.py
@@ -0,0 +1,11 @@
+from bun import Bun
+
+class TestBun():
+
+    def test_get_name_return_name(self):
+        bun = Bun('name', 123)
+        assert bun.get_name() == 'name'
+
+    def test_get_price_return_value(self):
+        bun = Bun('name', 123)
+        assert bun.get_price() == 123
